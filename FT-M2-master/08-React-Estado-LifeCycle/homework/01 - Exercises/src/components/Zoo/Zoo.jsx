c95ecb1e7bbe1ed69e6e50c7eaec20f536f7a424
Dalemenver <dalemenver@hotmail.com>
2023-09-23 22:09:53 -0500

    Avance de trabajo

--- FT-M2-master/08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
+++ FT-M2-master/08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
@@ -7,11 +7,54 @@ import Species from "../Species/Species";
 
 export default function Zoo() {
   /* Escribe acá tu código */
+  const [zoo, setZoo] = React.useState({
+    zooName: '',
+    animals: [],
+    species: [],
+    allAnimals: '',
+  });
+  //Funciones
+  const handleInputchange = (event) => {
+    setZoo({
+      ...zoo,
+      zooName: event.target.value
+    })
+  }
+  React.useEffect(()=>{
+    //fetch obligatorio
+  fetch('http://localhost:3001/zoo')
+  .then((res) => res.json())
+  .then((data) =>
+     setZoo({
+        ...zoo,
+        animals: data.animals,
+        species: data.species,
+        allAnimals: data.animals,
+     })
+  )
+  .catch((error) => console.log(error));
 
+  }, [])
 
+  const handleSpecies = (evento) => {
+    setZoo({
+      ...zoo,
+      animals: 
+    })
+  }
+  const handleAllSpecies = () => {}
+  
   return (
     <div>
-      <h1>-</h1>
+      <label for="crete">Zoo Name: </label>
+      <input 
+        onChange={handleInputchange} 
+        type="text" 
+        value={zoo.zooName} 
+        id="crete" />
+      <h1>{zoo.zooName}</h1>
+      <Species species={zoo.species} handleAllSpecies={handleAllSpecies} handleSpecies={handleSpecies}/>
+      <Animals animals={zoo.animals}/>
     </div>
   );
 }
